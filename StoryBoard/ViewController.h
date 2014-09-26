19ea9a7569020abd884c3b6ffa48af6e375d221f
shanxu <kingundertree@163.com>
2014-09-26 14:04:30 +0800

    add project

---
+++ StoryBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryBoard
+//
+//  Created by xiazer on 14-9-26.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StoryBoard/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StoryBoard
+//
+//  Created by xiazer on 14-9-26.
+//  Copyright (c) 2014年 xiazer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
